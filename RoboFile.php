3a4ee6b491e72c69cba6ccfe1bb78bf71917cec7
Alexander Menk <alex@addismap.com>
2022-05-28 17:55:56 +0200

    Always run complete test suite, even if one fails

--- RoboFile.php
+++ RoboFile.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This is project's console commands configuration for Robo task runner.
  *
@@ -6,19 +7,49 @@
  */
 class RoboFile extends \Robo\Tasks
 {
+    public function __construct()
+    {
+        $this->stopOnFail();
+    }
+
     public function test()
     {
         $this->taskComposerInstall()->run();
-        $this->testStatic();
+
+        $this->stopOnFail(false);
+        $resultStatic = $this->testStatic();
+        $resultPhpunit = $this->testPhpunit();
+
+        if (!($resultStatic && $resultPhpunit)) {
+            throw new \Exception('At least one test failed');
+        }
     }
 
     public function testStatic()
     {
-        $this->testLint();
+        $this->stopOnFail(false);
+        $resultLint = $this->testLint();
+        $resultPhpstan = $this->testPhpstan();
+
+        if (!($resultLint && $resultPhpstan)) {
+            $this->say('<error>At least one static test failed</error>');
+            return false;
+        }
+        return true;
     }
 
     public function testLint()
     {
-        $this->_exec('vendor/bin/phpcs');
+        return $this->taskExec('vendor/bin/phpcs -s')->run()->wasSuccessful();
+    }
+
+    public function testPhpstan()
+    {
+        return $this->taskExec('vendor/bin/phpstan')->run()->wasSuccessful();
+    }
+
+    public function testPhpunit()
+    {
+        return $this->taskExec('vendor/bin/phpunit')->run()->wasSuccessful();
     }
 }
